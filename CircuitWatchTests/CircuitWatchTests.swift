dc435e0ce43ba1ecc3dbee175e7ad94354e4d25a
Hong, Chang Nam <hcn1519@gmail.com>
2017-02-22 16:20:55 +0900

    Initial Commit

---
+++ CircuitWatchTests/CircuitWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  CircuitWatchTests.swift
+//  CircuitWatchTests
+//
+//  Created by 홍창남 on 2017. 2. 22..
+//  Copyright © 2017년 홍창남. All rights reserved.
+//
+
+import XCTest
+@testable import CircuitWatch
+
+class CircuitWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
