a43f6f79710b8d225401c0bcba6bd6f87a33d758
Hong, Chang Nam <hcn1519@gmail.com>
2017-02-23 17:39:19 +0900

    mainVC setting

---
+++ CircuitWatch/CircuitCell.swift
@@ -0,0 +1,21 @@
+//
+//  CircuitCell.swift
+//  CircuitWatch
+//
+//  Created by 홍창남 on 2017. 2. 23..
+//  Copyright © 2017년 홍창남. All rights reserved.
+//
+
+import UIKit
+
+class CircuitCell: UITableViewCell {
+
+    @IBOutlet weak var trainingTitle: UILabel!
+    @IBOutlet weak var trainingTime: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CircuitWatch/CircuitVC.swift
+++ CircuitWatch/CircuitVC.swift
@@ -13,9 +13,19 @@ class CircuitVC: UIViewController {
     @IBOutlet weak var circuitProgressView: ProgressBar!
     @IBOutlet weak var testSlider: UISlider!
     
+    var timeSet: TimeCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.navigationController?.navigationBar.tintColor = .white
+        
+    }
+    func setUpTime() {
+        timeSet = TimeCell(prepareTime: 0, workoutTime: 0, workoutCount: 0, setCount: 0, workoutBreakTime: 0, setBreakTime: 0, wrapUpTime: 0)
+        
+        
+        
     }
     @IBAction func sliderMoved(_ sender: Any) {
         circuitProgressView.progress = CGFloat(testSlider.value)
